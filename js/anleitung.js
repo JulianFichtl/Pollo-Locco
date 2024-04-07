8a06c096e84010b30a2a6f5306f78305b95ffdfe
Julian Fichtl <64958181+JulianFichtl@users.noreply.github.com>
2024-04-07 14:21:24 +0200

    added anleitung overlay

---
+++ js/anleitung.js
@@ -0,0 +1,6 @@
+function anleitungOpen() {
+    document.getElementById("anleitungTextWrapper").classList.remove("d-none");
+}
+function anleitungClose() {
+    document.getElementById("anleitungTextWrapper").classList.add("d-none");
+}
